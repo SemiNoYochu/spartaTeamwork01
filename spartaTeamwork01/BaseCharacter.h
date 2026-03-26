f75fbef0af3ee2f29c77f76219b1f387005abcd6
onlySaying <48788534+onlySaying@users.noreply.github.com>
2026-03-26 17:45:11 +0900

    push

---
+++ spartaTeamwork01/BaseCharacter.h
@@ -0,0 +1,20 @@
+#pragma once
+#pragma once
+
+#include "Actor.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class BaseCharacter : public Actor
+{
+private:
+	string name;
+public:
+	BaseCharacter();
+	~BaseCharacter();
+	void attack() override;
+	void defend() override;
+	void heal() override;
+};
